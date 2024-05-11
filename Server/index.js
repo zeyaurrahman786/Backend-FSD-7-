efa6ac49d88a66c7c73ac8d7b14acbdf4ae5d26a
zeyaurrahman786 <zeyaurrahman078608@gmail.com>
2024-05-11 18:36:11 +0530

    Node.js

--- Server/index.js
+++ Server/index.js
@@ -5,9 +5,83 @@
 
 
 
-let joke = require ('give-me-a-joke')
-console.log(joke);
+// let joke = require ('give-me-a-joke')
+// console.log(joke);
 
-joke.getRandomDadJoke ((jokes) => {
-    console.log(jokes);
+// joke.getRandomDadJoke ((jokes) => {
+//     console.log(jokes);
+// })
+
+
+
+
+
+
+// Asynchronous Methods :----------------------------------------------------------------
+
+// Create File :-)
+
+// let fs = require ("fs");
+// fs.writeFileSync("File.txt", "Hello world!");
+
+
+
+
+
+// Append File :-)
+
+// let fs = require ("fs");
+// fs.appendFileSync("NewFile.txt", " How are you ?");
+
+
+
+
+
+// Rename File :-)
+
+// let fs = require ("fs");
+// fs.renameSync("File.txt", "NewFile.txt");
+
+
+
+
+
+// Read File :-)
+
+// let fs = require ("fs");
+// let data = fs.readFileSync("File.txt");
+// console.log(data.toString);
+
+
+
+
+// Delete File :-)
+
+// let fs = require ("fs");
+// fs.unlinkSync("NewFile.txt");
+
+
+
+
+
+
+// Synchronous Methods :----------------------------------------------------------------
+
+// Create File :-)
+
+let fs = require("fs");
+fs.writeFile("index.html", "<h1> Hello World </h1>", (err) =>{
+    if(err) throw err;
+    console.log("File created successfully");
 })
+
+
+
+
+
+// Read File :-)
+
+// fs.readFile("index.html", (err, data) => {
+//     if(err) throw err;
+//     console.log(data.toString());
+// })
