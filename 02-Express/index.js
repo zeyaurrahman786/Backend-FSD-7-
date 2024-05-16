30cbab67d0fe0ee9e7e3832b1749783b9183e26b
zeyaurrahman786 <zeyaurrahman078608@gmail.com>
2024-05-17 00:56:21 +0530

    Backend

--- 02-Express/index.js
+++ 02-Express/index.js
@@ -1,38 +1,139 @@
+// let express = require('express');
+
+// let app = express();    
+
+// // app.use((req,res) =>{
+// //     res.send('Hello This is our server!!!!');
+// // })
+
+
+// // Routing :-)
+
+// app.get('/', (req, res)=>{
+//     res.send("Zeyaur Rahman")
+// })
+
+// app.get('/home', (req, res)=>{
+//     res.send("Welcome to my Home !")
+// })
+
+// app.get('/profile', (req, res)=>{
+//     res.send("Welcome to my profile !")
+// })
+
+// app.get('/about', (req, res)=>{
+//     res.send("Do you know about me !")
+// })
+
+// app.get('/home/profile/about', (req, res)=>{
+//     res.send("I am a Web Developer")
+// })
+
+// // app.get('*', (req, res)=>{
+// //     res.send("Matlab Kuch bhi !")
+// // })
+
+// app.listen(3000,()=>{
+//     console.log('Server is running on port 3000');
+// })
+
+
+
+
+
+
+
+
+
+
+
+// // Dynamic Routing :-)
+
+// let express = require('express');
+
+// let app = express();    
+
+// app.get('/', function(req, res){
+//     res.send("Hello World!");
+// })
+
+// // app.get('/:r/:a', (req, res) => {
+// //     let {a} = req.params;
+// //     console.log(req.params, "Hiiiiiii");
+// //     res.send(a);
+// // })
+
+// app.get('/:r/:a', (req, res) => {
+//     let {r,a} = req.params;
+//     res.send(`${r} ${a}`); 
+// })
+
+// app.listen(3000, (req, res) => {
+//     console.log("Server listening on port 3000");
+// });
+
+
+
+
+
+
+
+
+
+
+
+// // Query Parameters :-)
+
+// let express = require('express');
+
+// let app = express();    
+
+// // app.get('/search', function(req, res){
+// //     console.log("Searching...");
+// //     console.log(req.query);
+// //     res.send("Hello World!");
+// // })
+
+// app.get('/search', function(req, res){
+//     console.log("Searching...");
+//     console.log(req.query);
+//     let {firstName, lastName} = req.query;
+//     res.send(`${firstName} ${lastName}`);
+// })
+
+// app.listen(3000, (req, res) => {
+//     console.log("Server listening on port 3000");
+// });
+
+
+
+// // http://localhost:3000/search?firstName=Zeyaur&lastName=Rahman
+// // { firstName: 'Zeyaur', lastName: 'Rahman' }
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Server-Side-Rendering :-)
+
 let express = require('express');
 
 let app = express();    
 
-// app.use((req,res) =>{
-//     res.send('Hello This is our server!!!!');
-// })
+app.set('view engine', 'ejs');
 
-
-// Routing
-
-app.get('/', (req, res)=>{
-    res.send("Zeyaur Rahman")
+app.get('/', (req, res) =>{
+    res.render('index');
 })
 
-app.get('/home', (req, res)=>{
-    res.send("Welcome to my Home !")
-})
-
-app.get('/profile', (req, res)=>{
-    res.send("Welcome to my profile !")
-})
-
-app.get('/about', (req, res)=>{
-    res.send("Do you know about me !")
-})
-
-app.get('/home/profile/about', (req, res)=>{
-    res.send("I am a Web Developer")
-})
-
-// app.get('*', (req, res)=>{
-//     res.send("Matlab Kuch bhi !")
-// })
-
-app.listen(3000,()=>{
-    console.log('Server is running on port 3000');
-})
+app.listen(3000, (req, res) => {
+    console.log("Server listening on port 3000");
+});
