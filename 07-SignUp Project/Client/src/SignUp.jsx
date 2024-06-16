ee2c35e2db27d3e638d58739d95455111df87033
zeyaurrahman786 <zeyaurrahman078608@gmail.com>
2024-06-16 22:28:30 +0530

    SignUp LogIn Project

--- 07-SignUp Project/Client/src/SignUp.jsx
+++ 07-SignUp Project/Client/src/SignUp.jsx
@@ -1,53 +1,42 @@
-/* eslint-disable no-unused-vars */
-import React from 'react'
+// eslint-disable-next-line no-unused-vars
+import React, { useState } from 'react'
+import axios from 'axios'
 
-const SignUp = () => {
-    return (
-        <>
-            <form onSubmit={handleSubmit}>
-                <label>
-                    First Name:
-                    <input
-                        type="text"
-                        name="firstName"
-                        value={formData.firstName}
-                        onChange={handleChange}
-                    />
-                </label>
-                <br />
-                <label>
-                    Last Name:
-                    <input
-                        type="text"
-                        name="lastName"
-                        value={formData.lastName}
-                        onChange={handleChange}
-                    />
-                </label>
-                <br />
-                <label>
-                    Email:
-                    <input
-                        type="email"
-                        name="email"
-                        value={formData.email}
-                        onChange={handleChange}
-                    />
-                </label>
-                <br />
-                <label>
-                    Message:
-                    <textarea
-                        name="message"
-                        value={formData.message}
-                        onChange={handleChange}
-                    />
-                </label>
-                <br />
-                <button type="submit">Submit</button>
-            </form>
-        </>
-    )
+const Signup = () => {
+  let [input, SetInput] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: ""
+  })
+
+  function fun1(e) {
+    let { name, value } = e.target
+    SetInput({ ...input, [name]: value })
+  }
+
+  async function done(e) {
+    e.preventDefault();
+    console.log('click');
+    let res = await axios.post('http://localhost:5000/signup', input)
+    console.log(res, "hehehehehe");
+  }
+
+  return (
+    <div>
+      <form onSubmit={done}>
+        <div id="form">
+          <div className="from2">
+            <input className="forminput" name='firstName' value={input.firstName} onChange={fun1} type="text" placeholder="First name " />
+            <input className="forminput" name='lastName' value={input.lastName} onChange={fun1} type="text" placeholder="last name " />
+            <input className="forminput" name='email' value={input.email} onChange={fun1} type="Email" placeholder="Email" />
+            <input className="forminput" name='passWord' value={input.passWord} onChange={fun1} type="password" placeholder="PassWord " />
+            <button className="formbtn" >Create Account</button>
+          </div>
+        </div>
+      </form>
+    </div>
+  )
 }
 
-export default SignUp;
+export default Signup;
